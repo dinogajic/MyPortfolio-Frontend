2d4a0ce03b934258fc3ce7e63c760e0a077cc64d
unknown <rboric1901@gmail.com>
2022-08-23 19:42:56 +0200

    Password change fix

---
+++ myportfolio/src/components/DialogChoice.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="text-center">
+    <v-dialog v-model="compDialog" width="500">
+      <v-card>
+        <v-card-title class="text-h5 grey lighten-2">
+          Notification
+        </v-card-title>
+
+        <v-divider></v-divider>
+
+        <v-card-text color="green" text>{{ alertChoiceData }}</v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            color="primary"
+            text
+            @click="
+              compDialog = false;
+              resetPassword();
+            "
+          >
+            YES
+          </v-btn>
+          <v-btn
+            color="red"
+            text
+            @click="
+              compDialog = false;
+              refreshPage();
+            "
+          >
+            NO
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["alertChoiceData", "choiceDialog", "resetPassword"],
+  name: "Dialog",
+  data() {
+    return {
+      compDialog: this.choiceDialog,
+    };
+  },
+  methods: {
+    refreshPage() {
+      this.$router.go();
+    },
+  },
+  watch: {
+    alertChoiceData() {
+      this.compDialog = true;
+    },
+  },
+};
+</script>
